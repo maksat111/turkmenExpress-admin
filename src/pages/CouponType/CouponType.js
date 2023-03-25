cd5dd3e1f15df1b28d3b684a1176cc2adea02e73
maksat111 <maksatakmyradow111@gmail.com>
2023-03-25 17:57:40 +0500

    coupen type

--- src/pages/CouponType/CouponType.js
+++ src/pages/CouponType/CouponType.js
@@ -1,11 +1,227 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
+import { Modal } from 'antd';
+import { Checkbox, message } from 'antd';
+import { axiosInstance } from '../../config/axios';
+import TableComponent from '../../components/TableComponent';
+import Input from 'antd/es/input/Input';
 
-function CouponType(props) {
+function DeliveryType() {
+    const [dataSource, setDataSource] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [confirmLoading, setConfirmLoading] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [addOpen, setAddOpen] = useState(false);
+    const [newItem, setNewItem] = useState({
+        name_ru: '',
+        name_en: '',
+        name_tk: '',
+    })
+
+
+    const showModal = (item) => {
+        setOpen(true);
+        setSelectedItem(item);
+    };
+
+    const handleOk = async () => {
+        try {
+            setConfirmLoading(true);
+            await axiosInstance.delete(`coupon-type/delete/${selectedItem.id}`);
+            const newDataSource = dataSource.filter(element => element.id !== selectedItem.id);
+            setDataSource(newDataSource);
+            message.success('Успешно удалено')
+            setOpen(false);
+            setConfirmLoading(false);
+        } catch (err) {
+            setConfirmLoading(false)
+            message.error('Произошла ошибка. Пожалуйста, попробуйте еще раз!')
+            console.log(err)
+        }
+    };
+
+    const handleCancel = () => {
+        setOpen(false);
+    };
+
+    useEffect(() => {
+        axiosInstance.get('regions/list').then(res => {
+            res.data?.forEach(element => {
+                element.key = element.id
+            });
+            setDataSource(res?.data);
+        }).catch(err => console.log(err));
+    }, []);
+
+    const columns = [
+        {
+            title: 'id',
+            dataIndex: 'id',
+            key: 'id',
+            width: '50px',
+            style: { alignItems: "center" }
+        },
+        {
+            title: 'Номер',
+            dataIndex: 'number',
+            key: 'number',
+        },
+        {
+            title: 'Название (рус.)',
+            dataIndex: 'name_ru',
+            key: 'name_ru',
+        },
+        {
+            title: 'Название (туркм.)',
+            dataIndex: 'name_tk',
+            key: 'name_tk',
+        },
+        {
+            title: 'Навзание (анг.)',
+            dataIndex: 'name_en',
+            key: 'name_en',
+        },
+        {
+            title: 'От числа',
+            dataIndex: 'date_from',
+            key: 'date_from',
+        },
+        {
+            title: 'До числа',
+            dataIndex: 'to_date',
+            key: 'to_date',
+        },
+        {
+            title: 'Удалить',
+            dataIndex: 'active',
+            key: 'active',
+            render: (_, record) => (
+                <div className='delete-icon' onClick={() => showModal(record)}>
+                    Удалить
+                </div>
+            ),
+        },
+        {
+            title: 'Изменить',
+            dataIndex: 'active',
+            key: 'active',
+            render: (_, record) => (
+                <div className='update-icon' onClick={() => showAddModal(record)}>
+                    Изменить
+                </div>
+            ),
+        },
+    ];
+
+    //---------------------------------------------------ADD MODAL-------------------------------------------//
+    const showAddModal = (item) => {
+        // setSelectedItem(item);
+        item.id && setNewItem(item);
+        setAddOpen(true);
+    };
+
+    const handleAddOk = async () => {
+        setConfirmLoading(true);
+        const formData = new FormData();
+        const keys = Object.keys(newItem);
+        const values = Object.values(newItem);
+        keys.forEach((key, index) => {
+            formData.append(key, values[index]);
+        })
+        try {
+            if (newItem.id) {
+                const res = await axiosInstance.put(`regions/update/${newItem.id}/`, formData);
+                const index = dataSource.findIndex(item => item.id == newItem.id);
+                setDataSource(previousState => {
+                    const a = previousState;
+                    a[index].name_ru = newItem.name_ru;
+                    a[index].name_en = newItem.name_en;
+                    a[index].name_tk = newItem.name_tk;
+                    return a;
+                })
+            } else {
+                const res = await axiosInstance.post('regions/add/', formData);
+                setDataSource([...dataSource, newItem])
+            }
+            setConfirmLoading(false);
+            message.success('Успешно')
+            setAddOpen(false);
+        } catch (err) {
+            setConfirmLoading(false)
+            message.error('Произошла ошибка. Пожалуйста, попробуйте еще раз!')
+            console.log(err)
+        }
+    };
+
+    const handleAddCancel = () => {
+        setAddOpen(false);
+    };
+
+    const handleAddChange = (e) => {
+        setNewItem({ ...newItem, [e.target.name]: [e.target.value] });
+    }
+
     return (
-        <div className='page'>
-
-        </div>
+        <>
+            <Modal
+                title="Дополните детали"
+                open={addOpen}
+                onOk={handleAddOk}
+                confirmLoading={confirmLoading}
+                onCancel={handleAddCancel}
+                cancelText={'Отмена'}
+                okText={'Да'}
+                width={'600px'}
+                okType={'primary'}
+                style={{ top: '150px' }}
+            >
+                <div className='banner-add-container'>
+                    <div className='add-left'>
+                        <div className='add-column'>
+                            Название (рус.):
+                        </div>
+                        <div className='add-column'>
+                            Название (туркм.):
+                        </div>
+                        <div className='add-column'>
+                            Навзание (анг.):
+                        </div>
+                    </div>
+                    <div className='add-right'>
+                        <div className='add-column'>
+                            <Input name='name_ru' placeholder='Название (рус.)' value={newItem.name_ru} onChange={handleAddChange} />
+                        </div>
+                        <div className='add-column'>
+                            <Input name='name_tk' placeholder='Название (туркм.)' value={newItem.name_tk} onChange={handleAddChange} />
+                        </div>
+                        <div className='add-column'>
+                            <Input name='name_en' placeholder='Название (анг.)' value={newItem.name_en} onChange={handleAddChange} />
+                        </div>
+                    </div>
+                </div>
+            </Modal>
+            <Modal
+                title="Вы уверены, что хотите удалить?"
+                open={open}
+                onOk={handleOk}
+                confirmLoading={confirmLoading}
+                onCancel={handleCancel}
+                cancelText={'Отмена'}
+                okText={'Да'}
+                okType={'primary'}
+                okButtonProps={{ danger: true }}
+                style={{
+                    top: '200px'
+                }}
+            />
+            <div className='page'>
+                <div className='page-header-content'>
+                    <h2>Вид доставки</h2>
+                    <div className='add-button' onClick={showAddModal}>Добавлять</div>
+                </div>
+                <TableComponent dataSource={dataSource} columns={columns} pagination={false} />
+            </div>
+        </>
     );
 }
 
-export default CouponType;
+export default DeliveryType;
