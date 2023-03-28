e3773f75294d3e9c658a7930259e96dc7b17b785
maksat111 <maksatakmyradow111@gmail.com>
2023-03-28 06:32:15 +0500

    SettingsList page change

--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -3,7 +3,12 @@ import React from 'react';
 
 function TableComponent({ dataSource, columns, pagination, active }) {
     return (
-        <Table rowClassName={(record, rowIndex) => record.id == active && 'active-row'} dataSource={dataSource} columns={columns} pagination={pagination} />
+        <Table
+            rowClassName={(record, rowIndex) => record.id == active && 'active-row'}
+            dataSource={dataSource}
+            columns={columns}
+            pagination={pagination}
+        />
     );
 }
 
--- src/pages/SettingsList/SettingsList.js
+++ src/pages/SettingsList/SettingsList.js
@@ -6,49 +6,35 @@ import { PlusOutlined } from '@ant-design/icons';
 import './SettingsList.css';
 import Input from 'antd/es/input/Input';
 
-const getBase64 = (file) =>
-    new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = (error) => reject(error);
-    });
-
 function SettingsList() {
     const [dataSource, setDataSource] = useState([]);
     const [open, setOpen] = useState(false);
     const [confirmLoading, setConfirmLoading] = useState(false);
     const [selectedItem, setSelectedItem] = useState(null);
     const [total, setTotal] = useState(null);
-    const [progress, setProgress] = useState(0);
-    const [fileList, setFileList] = useState([]);
     const [addOpen, setAddOpen] = useState(false);
-    const [previewOpen, setPreviewOpen] = useState(false);
     const [selectOptions, setSelectOptions] = useState(null);
-    const [previewImage, setPreviewImage] = useState('');
-    const [previewTitle, setPreviewTitle] = useState('');
-    const [newItemCategory, setNewItemCategory] = useState([])
+    const [selectedGroupOption, setSelectedGroupOption] = useState([])
     const [newItem, setNewItem] = useState(null)
 
     useEffect(() => {
-        axiosInstance.get('subcategories/list').then(async (res) => {
+        axiosInstance.get('options/list').then(async (res) => {
             let a = [];
             let b = [];
             setTotal(res.data.count);
-            res.data?.results.forEach(element => {
+            res.data?.forEach(element => {
                 a.push({
                     id: element.id,
                     key: element.id,
                     name_ru: element.name_ru,
                     name_en: element.name_en,
                     name_tk: element.name_tk,
-                    image: element.image,
-                    category: element.category.name_ru
+                    option_group: element.option_group.name_ru
                 })
             });
             setDataSource(a);
-            const categories = await axiosInstance.get('categories/list/');
-            categories.data?.forEach(item => {
+            const groupOptions = await axiosInstance.get('options-group/list/');
+            groupOptions.data?.forEach(item => {
                 b.push({
                     label: item.name_ru,
                     value: item.name_ru,
@@ -75,29 +61,24 @@ function SettingsList() {
             title: 'Название (туркм.)',
             dataIndex: 'name_tk',
             key: 'name_tk',
+            width: '20%'
         },
         {
             title: 'Навзание (анг.)',
             dataIndex: 'name_en',
             key: 'name_en',
+            width: '20%'
         },
         {
-            title: 'Категория',
-            dataIndex: 'category',
-            key: 'category',
-        },
-        {
-            title: 'Logo',
-            dataIndex: 'image',
-            key: 'image',
-            render: (_, record) => (
-                <img className='subcategory-image' src={record.image} alt={record.name_ru} />
-            ),
+            title: 'Группа опций',
+            dataIndex: 'option_group',
+            key: 'option_group',
         },
         {
             title: 'Удалить',
             dataIndex: 'active',
             key: 'active',
+            width: '110px',
             render: (_, record) => (
                 <div className='delete-icon' onClick={() => showModal(record)}>
                     Удалить
@@ -108,6 +89,7 @@ function SettingsList() {
             title: 'Изменить',
             dataIndex: 'active',
             key: 'active',
+            width: '120px',
             render: (_, record) => (
                 <div className='update-icon' onClick={() => showAddModal(record)} >
                     Изменить
@@ -146,7 +128,7 @@ function SettingsList() {
         if (item.id) {
             setSelectedItem(item);
             const filtered = selectOptions.filter(category => category.label == item.category);
-            setNewItemCategory([{ id: filtered[0]?.id, label: item.category, value: item.category }])
+            setSelectedGroupOption([{ id: filtered[0]?.id, label: item.category, value: item.category }])
             setNewItem(item);
         }
         setAddOpen(true);
@@ -156,11 +138,10 @@ function SettingsList() {
         try {
             if (newItem.id) {
                 const formData = new FormData();
-                fileList[0] && formData.append("image", fileList[0].originFileObj, fileList[0].originFileObj.name);
                 formData.append("name_ru", newItem.name_ru);
                 formData.append("name_en", newItem.name_en);
                 formData.append("name_tk", newItem.name_tk);
-                formData.append('category', newItemCategory[0].id);
+                formData.append('category', selectedGroupOption[0].id);
                 const res = await axiosInstance.patch(`subcategoris/update/${newItem.id}/`, formData);
                 const index = dataSource.findIndex(item => item.id == newItem.id);
                 setDataSource(previousState => {
@@ -168,25 +149,21 @@ function SettingsList() {
                     a[index].name_ru = newItem.name_ru;
                     a[index].name_en = newItem.name_en;
                     a[index].name_tk = newItem.name_tk;
-                    a[index].image = fileList[0] ? URL.createObjectURL(fileList[0].originFileObj) : newItem.image;
-                    a[index].category = newItemCategory[0].value;
+                    a[index].category = selectedGroupOption[0].value;
                     return a;
                 })
             } else {
                 let a = [];
                 setConfirmLoading(true);
-                newItemCategory.forEach(async category => {
+                selectedGroupOption.forEach(async category => {
                     const formData = new FormData();
-                    formData.append("image", fileList[0].originFileObj, fileList[0].originFileObj.name);
                     formData.append("name_ru", newItem.name_ru);
                     formData.append("name_en", newItem.name_en);
                     formData.append("name_tk", newItem.name_tk);
                     formData.append('category', category.id);
                     const res = await axiosInstance.post(`subcategories/add/`, formData);
                     a.push({
-                        key: fileList[0].originFileObj.uid,
                         id: Math.floor(Math.random() * 1000),
-                        image: URL.createObjectURL(fileList[0].originFileObj),
                         name_ru: newItem.name_ru,
                         name_en: newItem.name_en,
                         name_tk: newItem.name_tk,
@@ -195,11 +172,10 @@ function SettingsList() {
                 })
                 setDataSource([...dataSource, ...a]);
             }
-            setNewItemCategory([]);
+            setSelectedGroupOption([]);
             setNewItem(null);
             message.success('Успешно добавлено');
             setAddOpen(false);
-            setFileList([]);
             setConfirmLoading(false);
         } catch (err) {
             setConfirmLoading(false)
@@ -209,57 +185,10 @@ function SettingsList() {
     };
 
     const handleAddCancel = () => {
-        setNewItemCategory([]);
-        setFileList([]);
+        setSelectedGroupOption([]);
         setNewItem(null);
         setAddOpen(false);
     };
-
-    const handleAddCustomRequest = async (options) => {
-        const { onSuccess, onError, file, onProgress } = options;
-
-        const config = {
-            headers: { "content-type": "multipart/form-data" },
-            onUploadProgress: event => {
-                const percent = Math.floor((event.loaded / event.total) * 100);
-                setProgress(percent);
-                if (percent === 100) {
-                    setTimeout(() => setProgress(0), 1000);
-                }
-                onProgress({ percent: (event.loaded / event.total) * 100 });
-            }
-        };
-        try {
-            onSuccess("Ok");
-        } catch (err) {
-            onError('Upload error');
-        }
-    };
-
-    //-----------------------------------------upload--------------------------------------------//
-    const handleChange = ({ fileList: newFileList }) => setFileList(newFileList);
-
-    const handlePreviewCancel = () => setPreviewOpen(false);
-
-    const handlePreview = async (file) => {
-        file.preview = await getBase64(file.originFileObj);
-        setPreviewImage(file.preview);
-        setPreviewOpen(true);
-        setPreviewTitle(file.name);
-    };
-
-    const uploadButton = (
-        <div>
-            <PlusOutlined />
-            <div
-                style={{
-                    marginTop: 8,
-                }}
-            >
-                Upload
-          </div>
-        </div>
-    );
 
     const handleAddChange = (e) => {
         setNewItem({ ...newItem, [e.target.name]: [e.target.value] });
@@ -288,7 +217,7 @@ function SettingsList() {
         selectOptions.forEach(item => {
             e.forEach(selected => item.value == selected && a.push({ id: item.id, label: selected, value: selected }));
         });
-        setNewItemCategory(a);
+        setSelectedGroupOption(a);
     }
 
     return (
@@ -317,10 +246,7 @@ function SettingsList() {
                             Навзание (анг.):
                         </div>
                         <div className='add-column'>
-                            Категория:
-                        </div>
-                        <div className='add-picture'>
-                            Logo
+                            Группа опций:
                         </div>
                     </div>
                     <div className='add-right'>
@@ -335,7 +261,7 @@ function SettingsList() {
                         </div>
                         <div className='add-column'>
                             <Select
-                                value={newItemCategory}
+                                value={selectedGroupOption}
                                 mode="multiple"
                                 allowClear
                                 style={{
@@ -345,18 +271,6 @@ function SettingsList() {
                                 onChange={(e) => handleUpdateSelectChange(e)}
                                 options={selectOptions}
                             />
-                        </div>
-                        <div className='add-picture'>
-                            {newItem?.id && <img className='subcategory-image' src={newItem?.image} alt={newItem?.name_ru} />}
-                            <Upload
-                                customRequest={handleAddCustomRequest}
-                                listType="picture-card"
-                                fileList={fileList}
-                                onPreview={handlePreview}
-                                onChange={handleChange}
-                            >
-                                {fileList.length == 0 && uploadButton}
-                            </Upload>
                         </div>
                     </div>
                 </div>
@@ -377,14 +291,16 @@ function SettingsList() {
             />
             <div className='page'>
                 <div className='page-header-content'>
-                    <h2>Подкатегории</h2>
+                    <h2>Список опций</h2>
                     <div className='add-button' onClick={showAddModal}>Добавлять</div>
                 </div>
                 <TableComponent
                     active={selectedItem?.id}
                     columns={columns}
                     dataSource={dataSource}
-                    pagination={{ onChange: onPaginationChange, total: total, pageSize: 20 }} />
+                    pagination={false}
+                // pagination={{ onChange: onPaginationChange, total: total, pageSize: 20 }} 
+                />
             </div>
         </>
     );
